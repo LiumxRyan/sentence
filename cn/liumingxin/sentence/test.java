9ea8fbd4328acb1bfbfbe5b453c2432939a9c415
LiuMingxin <1258854493@qq.com>
2022-07-27 09:20:46 +0800

    feat: paramUtil

---
+++ cn/liumingxin/sentence/test.java
@@ -0,0 +1,9 @@
+package cn.liumingxin.sentence;
+
+import cn.liumingxin.sentence.utils.ParamUtil;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(ParamUtil.getString("null"));
+    }
+}
---
+++ cn/liumingxin/sentence/utils/ParamUtil.java
@@ -0,0 +1,7 @@
+package cn.liumingxin.sentence.utils;
+
+public class ParamUtil {
+    public static String getString(String paramName) {
+        return paramName == null ? "" : paramName;
+    }
+}
